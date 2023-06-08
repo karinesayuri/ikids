734e04473d4ebd6c17f484192f85dadbbc443603
Karine Sayuri <karine.mine@hotmail.com>
2023-06-08 15:41:47 -0300

    adicionando endereço DAO

---
+++ Ikids/src/main/java/br/com/fiap/ikids/dao/EnderecoDAO.java
@@ -0,0 +1,22 @@
+package br.com.fiap.ikids.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class EnderecoDAO {
+
+	private final String ORACLE = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+
+	public Connection conect() {
+
+		try {
+			return DriverManager.getConnection(ORACLE, "rm97484", "");
+		} catch (SQLException e) {
+			System.out.println("Erro ao conectar");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+
+		}
+	}
+}

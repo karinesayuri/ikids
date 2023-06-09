b20f7f489afaa9638f6c9d4bceab8f1702d142f4
Karine Mine <karine.mine@hotmail.com>
2023-06-08 21:29:52 -0300

    adicionando pagina inicial do ikids

--- Ikids/src/main/java/br/com/fiap/ikids/controller/LoginController.java
+++ Ikids/src/main/java/br/com/fiap/ikids/controller/LoginController.java
@@ -7,13 +7,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/index")
+@WebServlet("/login")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.sendRedirect("index.jsp");
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        boolean autenticado = true;
+        
+        if (autenticado) {
+            response.sendRedirect("paginaSucesso.jsp");
+        } else {
+            response.sendRedirect("paginaErro.jsp");
+        }
+    }
 
 }

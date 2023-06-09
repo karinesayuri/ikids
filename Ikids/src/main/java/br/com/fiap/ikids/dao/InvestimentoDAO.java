d4752e395e269b51f400ab69cb5d1e9e5a6d146e
Karine Sayuri <karine.mine@hotmail.com>
2023-06-09 20:37:54 -0300

    adicionando modo exibir

--- Ikids/src/main/java/br/com/fiap/ikids/dao/InvestimentoDAO.java
+++ Ikids/src/main/java/br/com/fiap/ikids/dao/InvestimentoDAO.java
@@ -3,8 +3,11 @@ package br.com.fiap.ikids.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.fiap.ikids.factory.ConnectionFactory;
+import br.com.fiap.ikids.model.Endereco;
 import br.com.fiap.ikids.model.Investimento;
 
 public class InvestimentoDAO {
@@ -25,5 +28,22 @@ private Connection conexao;
 		
 		stmt.execute();
 		stmt.close();
+	}
+	
+	public List<Investimento> listaPorIdCliente(String idCliente) {
+		List<Investimento> clientes = new ArrayList<>();
+		String sql = "select select e.* from t_cliente c " + "left join t_invest e "
+				+ "on c.id_cliente = e.t_cliente_id_cliente " + "where c.id_cliente = ?";
+
+		try {
+			PreparedStatement stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, idCliente);
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return clientes;
+
 	}
 }
